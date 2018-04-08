285953b936285ecb00490c8f0c845e04d5eecf17
ivan <IvanIvanoff@users.noreply.github.com>
2018-04-08 18:04:43 +0700

    beautify printing (not really beautiful tho)

--- commit_crawler.sh
+++ commit_crawler.sh
@@ -2,17 +2,18 @@
 
 current_branch=`git rev-parse --abbrev-ref HEAD`
 current_commit=`git rev-parse --verify HEAD`
+counter=1
 
 while read -u 3 result
 do
   printf "\n"
   read -n 1 -s -r -p "Press any key to checkout the next commit" some_response
-  printf "\n"
+  printf "\n\n\n $counter. ======================================================================\n"
 
   git checkout $result | awk -F 'delim'\n\n'{print $1}' 
 
-  printf "\n"
-
+  printf "=========================================================================="
+  counter=$((counter+1))
 done 3< <(git log --pretty=oneline | cut -f1 -d" " | tac )
 
 read -n 1 -s -r -p "Press any key to where it all began" some_response
