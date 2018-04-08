aee90494aa6067c4f47b6c295bf6da60bf8b8096
ivan <IvanIvanoff@users.noreply.github.com>
2018-04-08 17:37:04 +0700

    Supress some git messages. Checkout master at the end

--- commit_crawler.sh
+++ commit_crawler.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-current_commit=git rev-parse --verify HEAD
-
 while read -u 3 result
 do
   printf "\n"
   read -n 1 -s -r -p "Press any key to checkout the next commit" some_response
-  git checkout $result
+  printf "\n"
+
+  git checkout $result | awk -F 'delim'\n\n'{print $1}' 
 
   printf "\n"
 
 done 3< <(git log --pretty=oneline | cut -f1 -d" " | tac )
+
+read -n 1 -s -r -p "Press any key to checkout master" some_response
+printf "\n"
+
+git checkout master | awk -F 'delim'\n\n'{print $1}'
